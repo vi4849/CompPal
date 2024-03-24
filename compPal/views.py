88f68f6237786b46a1b55e6e6cf37b1d4b3a0813
m-liv <miya.livingston@icloud.com>
2024-03-24 05:43:05 -0400

    working integration of html, css, and python

--- compPal/views.py
+++ compPal/views.py
@@ -17,19 +17,18 @@ def submit_survey(request):
         import json
         #post_data = json.loads(request.body.decode("utf-8"))
         memorySize = form.cleaned_data['memorySize']
-        #print(post_data)
         processor = form.cleaned_data['processor']
-        
-        #print(processor)
         brand = form.cleaned_data['brand']
         screen = form.cleaned_data['screen']
         price = form.cleaned_data['price']
         touch = form.cleaned_data['touch']
-        print(touch)
-
+        #print(touch)
 
         # Example: Perform some logic based on selected options
-        recommendation = scrape_best_buy_laptops(memorySize, processor, brand, screen, price,touch)
+        data_dict = scrape_best_buy_laptops()
+        knnFunction(data_dict)
+        #call makerecommendation(memorySize, processor, brand, screen, price,touch)
+        recommendation = "The intel one"
 
         response_data = {
             'success': True,
@@ -42,7 +41,8 @@ def submit_survey(request):
     
 
 #scrapes the first four pages of the best buy website 
-def scrape_best_buy_laptops(memorySize, processor, brand, screen, price,touch):
+#scrapes the first four pages of the best buy website 
+def scrape_best_buy_laptops():
     headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"} 
     urls = ['https://www.bestbuy.com/site/searchpage.jsp?id=pcat17071&st=laptops+computers+on+sale', 
             'https://www.bestbuy.com/site/searchpage.jsp?cp=2&id=pcat17071&st=laptops+computers+on+sale', 
@@ -55,19 +55,68 @@ def scrape_best_buy_laptops(memorySize, processor, brand, screen, price,touch):
         soup = BeautifulSoup(response.content, 'html.parser') # If this line causes an error, run 'pip install html5lib' or install html5lib
         # print(soup.prettify()) 
         for row in soup.findAll('h4', attrs = {'class':'sku-title'}):  # find the names of the laptops
-            laptop = {}
-            laptop['name'] = row.a.text
-            laptops.append(laptop) #add the key-value pairs (ex. name-"lenovo thinkpad...") to the dictionary
-    gb = []
-    for i in laptop:
-        name = i.get("name") #retrieves the value associated with the key
-        if (name.__contains__(memorySize)): #checks to see if the value contains the attribute
-            gb.append(name) #adds the laptop to the list 
-    processor = []
-    for i in gb:
-        if (i.__contains__(processor)): #checks to see if the value contains the attribute
-            gb.append(i) #adds the laptop to the list 
-    return processor
+            laptops.append(row.a.text) #add the key-value pairs (ex. name-"lenovo thinkpad...") to the dictionary
+
+    brand_list=[]
+    mem_list = []
+    processor_list = []
+    screen_list = []
+    touch_list = []
+
+    laptop_dictionary={"brand":brand_list, "memorySize":mem_list, "processor":processor_list, 
+                       "screen":screen_list, "touch": touch_list}
+
+    for i in laptops:
+        split = i.split('- ')
+        if(len(split)<5):
+            split = i.split(' -')
+
+        #print(len(split))
+        split_2 = split[1].split('"')
+        #for j in split:
+         #   print(j)
+        #correct up to this point 
+
+        brand_list.append(split[0])
+        if  len(split) < 5:
+            mem_list.append("N/A")
+        else:
+            mem_list.append(split[3])
+        processor_list.append(split[2])
+        screen_list.append(split_2[0])
+        if len(split_2)>1:
+            if "touch" in split_2[1].lower():
+                touch_list.append("true")
+            else:
+               touch_list.append("false")     
+        else:
+            touch_list.append("false")
+        print(laptop_dictionary)
+
+    
+    """
+    for key, value_list in laptop_dictionary.items():
+        print(f"Elements for {key}:")
+        for element in value_list:
+                print(element)
+        print("--------")
+    """
+    return laptop_dictionary
+
+def makeRecommendation(brand,memorySize, processor, screen,touch):
+    return "the intel one"
+
+def knnFunction(data_dict):
+    df = pd.DataFrame(data_dict)
+    df_encoded = pd.get_dummies(df, columns=['brand', 'memorySize','processor','screen','touch'])
+    
+
+
+
+#print(len(laptop_data))
+#print(type(laptop_data[0]))
+#laptop_data = scrape_best_buy_laptops() 
+#print(laptop_data)
 
 
 @require_http_methods(["GET"])
--- computer_recs/settings.py
+++ computer_recs/settings.py
@@ -16,6 +16,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'compPal/templates/resources'),
+]
 
 
 # Quick-start development settings - unsuitable for production
